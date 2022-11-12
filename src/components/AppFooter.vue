f0727a5b163c1d6f25fe127f91ade55bd0ab1357
Quentin <quentin@qcasts.com>
2022-11-13 00:11:52 +0100

    Dev Add some color (#9)
    Co-authored-by: Quentin <quentin@Quentins-MBP.fritz.box>

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-gray-900 py-3">
+    <div class="w-full text-center text-white">
+      &copy; Travelly Pty Ltd
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFooter"
+};
+</script>
